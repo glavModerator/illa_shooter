d197d32f2c17da172631e8a927a5382595dd7f61
glavModerator <ilahusnutdinov57@gmail.com>
2025-03-15 15:17:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from pygame import *
+from random import randint #⬅️
+
+# нам потрібні такі картинки:
+img_back = "galaxy.jpg"  # фон гри
+img_hero = "rocket.png"  # герой
+img_enemy = "ufo.png"  # ворог ⬅️
+img_bullet = "bullet.png" # куля
 
 
-img_back = "galaxy.jpg"
-img_hero = "rocket.png"
-
-
-win_winget = 700
-win_heiget = 500
+# створюємо віконце
+win_width = 700
+win_height = 500
 display.set_caption("Shooter")
-window = display.set_mode((win_winget, win_heiget))
-background = transform.scale(image.load(img_back), (win_winget, win_heiget))
-
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back),
+                             (win_width, win_height))
 
 finish = False
 run = True
@@ -20,10 +24,23 @@ mixer.music.load('space.ogg')
 mixer.music.play(-1)
 fire_sound = mixer.Sound('fire.ogg')
 
+# шрифти і написи⬅️
+font.init()
+font2 = font.Font(None, 36)
+
+
+score = 0  # збито кораблів
+lost = 0  # пропущено кораблів
+goal = 10
+max_lost = 3
+
+
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y,  # один рядок
+                 size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.scale(  # один рядок
+            image.load(player_image), (size_x, size_y))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -32,29 +49,87 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
+# клас головного гравця
 class Player(GameSprite):
+
+    # метод для керування спрайтом стрілками клавіатури
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_winget -80:
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
 
+    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
-ship = Player(img_hero, 5, win_heiget -100, 80, 100,10)
+
+# клас спрайта-ворога
+class Enemy(GameSprite):
+    # рух ворога
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        # зникає, якщо дійде до краю екрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+
+
+# створюємо спрайти
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+# створюємо ворогів ⬅️
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(
+        80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
+bullets = sprite.Group()
 
 while run:
+    # подія натискання на кнопку Закрити
     for e in event.get():
         if e.type == QUIT:
             run = False
 
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+
+
     if not finish:
         window.blit(background, (0, 0))
+
+        # пишемо текст на екрані⬅️
+        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
         ship.update()
         ship.reset()
 
+        monsters.update()
+        monsters.draw(window)
+
+        bullets.update()
+        bullets.draw(window)
 
         display.update()
+    # цикл спрацьовує кожні 0.05 секунд
     time.delay(50)
